e9c788ad7f2363f09b2a846b9dd21ce9595a3526
Matheus <contato.matheusdesantana@gmail.com>
2023-10-25 15:37:43 -0300

    Pieces can move now

--- src/App.java
+++ src/App.java
@@ -1,12 +1,27 @@
-import boadgame.Board;
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
+        Scanner read = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
 
         System.out.println();
-        UI.printBoard(chessMatch.getPieces());
+        while (true) {
+            UI.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.println("Source: ");
+            ChessPosition source = UI.readChessPosition(read);
+
+            System.out.println();
+            System.out.println("Target: ");
+            ChessPosition target = UI.readChessPosition(read);
+
+            ChessPiece capturedPiece = chessMatch.perfomeChessMove(source, target);
+        }
     }
 }
